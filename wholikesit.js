df7311b72b61426796e8abaad7f78dfacdf7f7ee
Rena Goldsmith <rena.kg@gmail.com>
2016-10-09 11:16:52 +1300

    Update wholikesit.js

--- wholikesit.js
+++ wholikesit.js
@@ -1,19 +1,13 @@
-function likes(names) {
-  var likesThis =  "likes this"
-
-  if (names.length == 1) {
-    return names[0] + " likes this"
-  } else if (names.length == 2) {
-     return names[0] + " and " + names[1] + " like this"
-    }
-    else if (names.length == 3) {
-      return names[0] + ", " + names[1] + " and " + names[2] + " like this"
-    }
-    else if (names.length > 3) {
-      var others = names.length - 2 + " others"
-      return names[0] + ", " + names[1] + " and " + others + " like this"
-    }
-    else {
-      return "no one likes this"
-    }
+likes = (names) => {
+  switch (names.length) {
+    case 0: return "no one likes this";
+      break;
+    case 1: return names[0] + " likes this";
+      break;
+    case 2: return names[0] + " and " + names[1] + " like this";
+      break;
+    case 3: return names[0] + ", " + names[1] + " and " + names[2] + " like this";
+      break;
+    default: return names[0] + ", " + names[1] + " and " + (names.length - 2) + " others like this";
+  }
 }
