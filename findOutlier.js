5cf373975d4d9cbdf63a640fa8f154c9b627b65d
Rena Goldsmith <rena.kg@gmail.com>
2016-10-11 00:25:49 +1300

    Update with best practice solution

--- findOutlier.js
+++ findOutlier.js
@@ -27,3 +27,12 @@ function findOutlier(integers) {
     
    return outlier
 }
+
+/* Best Practice Solution :) I knew you could use filter this way so why didn't I do that :/
+function findOutlier(int){
+  var even = int.filter(a=>a%2==0);
+  var odd = int.filter(a=>a%2!==0);
+  return even.length==1? even[0] : odd[0];
+}
+*/
+
