ef79abb3e8ffc3781ff385e7ecfe1f501ee0ab56
Rena Goldsmith <rena.kg@gmail.com>
2016-10-25 00:55:52 +1300

    Update knock.js

--- knock.js
+++ knock.js
@@ -1,1 +1,11 @@
 knock(knock)()()
+
+// below is what we get when we run knock.toString()
+
+function (arg) {
+     if (arg && arg === knock) {
+        console.log('"Knock knock."');
+        console.log('Whos\'s there?');
+        return deliverLine;
+      }
+    }
